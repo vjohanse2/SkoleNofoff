fa6103c1f5b33c399c814c4421c948bb7ba7f8ed
vjohanse2 <vegard.johansen@vegarddev.onmicrosoft.com>
2025-08-06 18:38:35 +0200

    ss

---
+++ PythonBasics.py
@@ -0,0 +1,49 @@
+## https://www.w3schools.com/python/python_conditions.asp
+
+# Vatriabler# Variables are containers for storing data values
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+print(x)
+print(y)
+print(z)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+# Funksjoner# Functions are blocks of code which only run when they are called
+
+def my_function():
+  print("Hello from a function")
+ my_function()   
+
